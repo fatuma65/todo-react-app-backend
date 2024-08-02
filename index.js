a202c2b6b2a893e3da792ff579df51f88f48588a
fatuma65 <namalefatuma28@gmail.com>
2024-08-02 11:00:24 +0300

    add create user endpoint

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const {PrismaClient} = require('@prisma/client')
-
+const userRoutes = require('./Routes/userRoutes')
+const port = process.env.PORT
 const app = express()
 const prisma = new PrismaClient()
 
 app.use(express.json())
 
+app.use('/users', userRoutes)
+app.listen(4000, () => {
+    console.log(`Server running on port ${port}`)
+})
